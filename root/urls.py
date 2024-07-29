b68783b65d1b0d587c5065546886a5a8ab02f12f
“krv006” <rvkamronbek@gmail.com>
2024-07-29 19:48:38 +0500

    jazzmin and ckditor add

--- root/urls.py
+++ root/urls.py
@@ -1,8 +1,13 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+from django.urls import path
 
+from root import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
