18580da42b1bf0fe9e949abe5994cbfab8075afb
krv006 <rvkamronbek@gmail.com>
2024-08-29 19:09:58 +0500

    requirements.txt add

--- apps/urls.py
+++ apps/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path("product/update/<int:pk>", ProductUpdateAPIView.as_view(), name='product-update-list-api-view'),
 
     path("debtors", DebtorsListCreateAPIView.as_view(), name='debtors-list-api-view'),
-    path("debtors/<int:pk>", DebtorsUpdateAPIView.as_view(),name='debtors-update-list-api-view'),
+    path("debtors/<int:pk>", DebtorsUpdateAPIView.as_view(), name='debtors-update-list-api-view'),
 
-    path("warehouse", WarehouseListCreateAPIView.as_view(),name='warehouse-list-api-view'),
+    path("warehouse", WarehouseListCreateAPIView.as_view(), name='warehouse-list-api-view'),
 
     path('manager-admin/', ManagerAdminListCreateView.as_view(), name='manager-admin-list-create'),
     path('manager-admin/<int:pk>/', ManagerAdminRetrieveUpdateDestroyView.as_view(), name='manager-admin-detail'),
@@ -21,5 +21,4 @@ urlpatterns = [
     path('sale/', SaleListCreateAPIView.as_view(), name='sale'),
 
     path('daily-benefit/<str:date_str>/', DailyBenefitView.as_view(), name='daily_benefit'),
-
 ]
